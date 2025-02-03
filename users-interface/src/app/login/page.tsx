3d7af5599dcf9d20b6986fc8d3c0ed89de181268
Derek Galeas <dhenrygk12@gmail.com>
2025-02-03 16:35:18 -0600

    login page

---
+++ users-interface/src/app/login/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+
+const login = () => {
+
+  return (
+    <>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold">Login</h1>
+            <form className="flex flex-col space-y-4">
+            <input
+                type="text"
+                placeholder="Username"
+                className="p-2 border
+                border-gray-300 rounded"
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                className="p-2 border
+                border-gray-300 rounded"
+            />
+            <button
+                type="submit"
+                className="bg-blue-500 text-white p-2 rounded"
+            >
+                Login
+            </button>
+            </form>
+        </div>
+    </>
+  );
+};
+
+export default login;
