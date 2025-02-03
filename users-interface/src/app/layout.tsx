ace051c66b02a29b0886889b4fbdcb0cccea008e
Derek Galeas <dhenrygk12@gmail.com>
2025-02-03 11:20:07 -0600

    add providers

--- users-interface/src/app/layout.tsx
+++ users-interface/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Providers from "@/providers/providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,10 +32,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
-        <footer className="border-t border-gray-400 text-gray-400 py-4 text-center sticky bottom-0">
-          <p>© {getActualYear()} User App</p>
-        </footer>
+        <Providers>
+          {children}
+          <footer className="border-t border-gray-400 text-gray-400 py-4 text-center sticky bottom-0">
+            <p>© {getActualYear()} User App</p>
+          </footer>
+        </Providers>
       </body>
     </html>
   );
