75e000d0e34b735e05572d05e4542527a59733e1
Derek Galeas <dhenrygk12@gmail.com>
2025-02-04 15:09:11 -0600

    correction with ant design

--- users-interface/src/app/login/page.tsx
+++ users-interface/src/app/login/page.tsx
@@ -50,43 +50,40 @@ const Login: React.FC = () => {
   return (
     <div className="bg-white shadow-md rounded-lg p-8">
       <h1 className="text-4xl font-bold mb-5 text-center">Login</h1>
-      <Form
-        disabled={loading}
-        name="login"
-        layout="vertical"
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        style={{ width: "100%", maxWidth: 400 }}
-      >
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[
-            { required: true, message: "Please input your email!" },
-            { type: "email", message: "Please enter a valid email!" },
-          ]}
+      <Spin tip="Loading" size="large" spinning={loading}>
+        <Form
+          name="login"
+          layout="vertical"
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          style={{ width: "100%", maxWidth: 400 }}
         >
-          <Input placeholder="Enter your email" />
-        </Form.Item>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              { required: true, message: "Please input your email!" },
+              { type: "email", message: "Please enter a valid email!" },
+            ]}
+          >
+            <Input placeholder="Enter your email" />
+          </Form.Item>
 
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: "Please input your password!" }]}
-        >
-          <Input.Password placeholder="Enter your password" />
-        </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password placeholder="Enter your password" />
+          </Form.Item>
 
-        <Form.Item className="flex justify-center items-center">
-          {loading ? (
-            <Spin size="Large" />
-          ) : (
+          <Form.Item>
             <Button type="primary" htmlType="submit" block>
               Login
             </Button>
-          )}
-        </Form.Item>
-      </Form>
+          </Form.Item>
+        </Form>
+      </Spin>
     </div>
   );
 };
