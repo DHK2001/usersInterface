b4b7aa3ed78c59d32c563e09a667695d3bf56516
Derek Galeas <dhenrygk12@gmail.com>
2025-02-04 14:13:24 -0600

    add login.

--- users-interface/src/app/login/page.tsx
+++ users-interface/src/app/login/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { Button, Form, Input, message } from "antd";
+import { Button, Form, Input, message, Spin } from "antd";
 import { loginUser } from "@/services/apis/users-api";
 import { useRouter } from "next/navigation";
 
@@ -11,6 +11,7 @@ type LoginFormValues = {
 };
 
 const Login: React.FC = () => {
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
   const [messageApi, contextHolder] = message.useMessage();
   const [status, setStatus] = useState<{
@@ -30,14 +31,20 @@ const Login: React.FC = () => {
   }, [status, messageApi]);
 
   const onFinish = async (values: LoginFormValues) => {
+    setLoading(true);
     const loginR = await loginUser(values);
-
+    
     if (loginR.status === 200) {
       setStatus({ type: "success", content: "Login Successful" });
-      router.push(`/`);
+      setTimeout(() => {
+        setLoading(false);
+        router.push(`/`);
+      }, 1000)
     } else if (loginR.status === 401) {
+      setLoading(false);
       setStatus({ type: "error", content: "Login Failed. Incorrect password" });
     } else {
+      setLoading(false);
       setStatus({ type: "error", content: "Login Failed. User not found" });
     }
   };
@@ -50,38 +57,40 @@ const Login: React.FC = () => {
     <div className="bg-white shadow-md rounded-lg p-8">
       {contextHolder}
       <h1 className="text-4xl font-bold mb-5 text-center">Login</h1>
-      <Form
-        name="login"
-        layout="vertical"
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        style={{ width: "100%", maxWidth: 400 }}
-      >
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[
-            { required: true, message: "Please input your email!" },
-            { type: "email", message: "Please enter a valid email!" },
-          ]}
+      <Spin tip="Loading" size="large" spinning={loading}>
+        <Form
+          name="login"
+          layout="vertical"
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          style={{ width: "100%", maxWidth: 400 }}
         >
-          <Input placeholder="Enter your email" />
-        </Form.Item>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              { required: true, message: "Please input your email!" },
+              { type: "email", message: "Please enter a valid email!" },
+            ]}
+          >
+            <Input placeholder="Enter your email" />
+          </Form.Item>
 
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: "Please input your password!" }]}
-        >
-          <Input.Password placeholder="Enter your password" />
-        </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password placeholder="Enter your password" />
+          </Form.Item>
 
-        <Form.Item>
-          <Button type="primary" htmlType="submit" block>
-            Login
-          </Button>
-        </Form.Item>
-      </Form>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" block>
+              Login
+            </Button>
+          </Form.Item>
+        </Form>
+      </Spin>
     </div>
   );
 };
